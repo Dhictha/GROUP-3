313c82e036126aa1da20a4793a0309f938db228b
Habiba-Hussein <147278092+Habiba-Hussein@users.noreply.github.com>
2024-01-03 21:01:30 +0300

    Update main.py
    This Python script interacts with the Restcountries API to retrieve and display essential information about a specified country. It prompts the user to input the country's name, constructs the API endpoint, and performs a GET request. The HTTP status code is then printed, and relevant details are extracted and presented as a summary of key country information.

--- main.py
+++ main.py
@@ -12,3 +12,30 @@ print(response.status_code)
 data = response.json()
 
 pp(data)
+
+# Import necessary modules
+from pip._vendor import requests
+from pprint import pprint as pp
+
+# Prompt user for the name of the country
+name = input("Enter the name of the country: ")
+
+# Construct the API endpoint for the given country
+endpoint = f"https://restcountries.com/v3.1/name/{name}?fullText=true"
+
+# Make the GET request to the API
+response = requests.get(endpoint)
+
+# Print the HTTP status code received from the API
+print(response.status_code)
+
+# Parse the JSON response data
+data = response.json()
+
+# Extract specific information from the response data
+area_value = data[0]['area']
+Population = data[0]['population']
+
+# Print information about the country
+print("Area:", area_value)
+print("Population:", Population)

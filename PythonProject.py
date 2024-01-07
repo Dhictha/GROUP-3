fc0cec25a2b24dcc037d61f4f275659ef3b05a3b
Jerydae <153497287+Jerydae@users.noreply.github.com>
2024-01-08 00:27:55 +0300

    Update PythonProject.py

--- PythonProject.py
+++ PythonProject.py
@@ -104,3 +104,17 @@ Population = data[0]['population']
 print("Area:", area_value)
 print("Population:", Population)
 
+#Retrieving data from the COVID-19 API
+from pip._vendor import requests
+from pprint import pprint as pp
+ 
+country = input("Enter the country of your choice: ")
+endpoint_url = f"https://disease.sh/v3/covid-19/countries/{country}"
+ 
+response = requests.get(endpoint_url)
+print(response.status_code)
+ 
+data = response.json()
+ 
+pp(data)
+
